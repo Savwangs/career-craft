c42a1cdbbe9913b9a4cb5998a87b1963620632e8
Savir Wangoo <savirwangoo@Savirs-MacBook-Air.local>
2025-01-07 19:44:19 -0800

    initializes firebase

--- career-craft-frontend/src/firebase.js
+++ career-craft-frontend/src/firebase.js
@@ -1,26 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";  // Firebase initialization
-import { getAuth } from "firebase/auth";  // Firebase Auth
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCQiwujHgUl0w4gtuQQzW_Og_-DxPFOI_c",
-  authDomain: "career-craft-98538.firebaseapp.com",
-  projectId: "career-craft-98538",
-  storageBucket: "career-craft-98538.firebasestorage.app",
-  messagingSenderId: "956492797583",
-  appId: "1:956492797583:web:d7aa8521a0872a5e2842fa",
-  measurementId: "G-QVZ8Z3MV8R"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication
 const auth = getAuth(app);
 
-// Export the services
 export { auth };
